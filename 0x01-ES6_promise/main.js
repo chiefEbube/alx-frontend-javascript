437e50034afde136a32dcaeeb179ba50a9840439
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-07 18:47:17 +0100

    task 3

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,4 +1,4 @@
-import getFullResponseFromAPI from './1-promise';
+import handleResponseFromAPI from './2-then';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
