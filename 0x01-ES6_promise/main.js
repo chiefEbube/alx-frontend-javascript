312ee57535a3847f63f63326fa24fa52ac39dac8
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-07 19:27:45 +0100

    task 4

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,4 +1,3 @@
-import handleResponseFromAPI from './2-then';
+import handleProfileSignup from './3-all';
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+handleProfileSignup();
