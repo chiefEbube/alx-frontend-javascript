0d1102302dd621596aa3e555075e1dcd1314f99a
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-23 21:00:38 +0100

    task 5

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,6 +1,14 @@
-import Pricing from './4-pricing.js';
-import Currency from './3-currency.js';
+import Building from './5-building.js';
 
-const p = new Pricing(100, new Currency("EUR", "Euro"))
-console.log(p);
-console.log(p.displayFullPrice());
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+    new TestBuilding(200)
+}
+catch(err) {
+    console.log(err);
+}
+
