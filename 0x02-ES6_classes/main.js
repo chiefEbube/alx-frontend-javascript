793f5aac0ddd6559a9375a2c6b0f16efd90d9da4
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-23 01:45:11 +0100

    task 2

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,4 +1,3 @@
-import ClassRoom from "./0-classroom.js";
+import initializeRooms from './1-make_classrooms.js';
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+console.log(initializeRooms());
