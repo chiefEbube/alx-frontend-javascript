2ea83d97624f16274b692c7fe15879a96bcd3f1b
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-23 02:56:53 +0100

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currenct must be an instance of class Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currenct must be an instance of class Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,4 +1,6 @@
-import Currency from "./3-currency.js";
+import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
 
-const dollar = new Currency('$', 'Dollars');
-console.log(dollar.displayFullCurrency());
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+console.log(p.displayFullPrice());
