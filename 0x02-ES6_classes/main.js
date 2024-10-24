84ca8d27440af7282b98b016a9ac6b7b128b976f
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-10-24 01:12:11 +0100

    task 9

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,5 +1,9 @@
-import HolbertonClass from "./8-hbtn_class.js";
+import listOfStudents from './9-hoisting';
 
-const hc = new HolbertonClass(12, "Mezzanine")
-console.log(Number(hc));
-console.log(String(hc));
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+  (student) => student.fullStudentDescription,
+);
+
+console.log(listPrinted);
